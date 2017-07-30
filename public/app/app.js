a4d487031b9312f7a7ae29dbb10ee56c9e082ee0
Karl74 <carlos.castro74@gmail.com>
2017-07-30 16:12:21 -0500

    app.js added, skillhtm style

---
+++ public/app/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  console.log("It's alive!!!!!");
+$(".pill").on("click", function(){
+  console.log("Hola Marianita, ¿Estas leyendo?");
+});
+
+});//End of document get ready
