a1097960d0a5105d64240bb3deb364d049904d6c
Karl74 <carlos.castro74@gmail.com>
2017-08-07 21:35:04 -0500

    find modal added

--- public/app/app.js
+++ public/app/app.js
@@ -1,47 +1,67 @@
-$(document).ready(function(){
+$(document).ready(function() {
   console.log("It's alive!!!!!");
+// == | A | EVENT HANDLERS ==================================
+  $(".headBar").on("click", function() {
+    if ($(event.target).data("status") == "contracted") {
+      expandChild($(event.target));
+    } else if ($(event.target).data("status") == "expanded") {
+      contractChild($(event.target));
+    }
+  });
+
+  $(".closeM").on("click", function(){
+      closeModal();
+  });
+
+  $(".find-pill").on("click", function(){
+    openModal();
+  });
+  // Make modal visible
+  function openModal(){
+    $(".modal-screen").css("display", "block");
+  }
+
+  // close modal (make it invisible)
+  function closeModal(){
+    $(".modal-screen").css("display", "none");
+  }
 
 
-
-$(".headBar").on("click", function(){
-  if($(event.target).data("status") == "contracted"){
-    expandChild($(event.target));
-} else if($(event.target).data("status") == "expanded"){
-    contractChild($(event.target));
-}
-});
-
+  // Expand a child
+  //   change the status at the buttonbar
+  //   change the bar class
+  //   change the display of the child
+  function expandChild(bar) {
+    bar.data("status", "expanded");
+    bar.attr("class", "childExpanded");
+    bar.parent().next().css("display", "block");
+    bar.children().attr("class", "glyphicon glyphicon-minus");
+  }
 // Expand a child
-      //   change the status at the buttonbar
-      //   change the bar class
-      //   change the display of the child
-function expandChild(bar){
-  bar.data("status", "expanded");
-  bar.attr("class", "childExpanded");
-  bar.parent().next().css("display", "block");
-  bar.children().attr("class", "glyphicon glyphicon-minus");
+  function contractChild(bar) {
+    bar.data("status", "contracted");
+    bar.attr("class", "window-title expander");
+    bar.parent().next().css("display", "none");
+    bar.children().attr("class", "glyphicon glyphicon-plus");
+  }
+
+// creates an student yellow pill
+  function studentPill(stuName, parent){
+    var pillStu = $("<div>");
+    pillStu.attr("class", "student-pill pill");
+    pillStu.html(stuName);
+    parent.append(pillStu);
+  }
+
+// studentPill("Marcela", $(".stu-dis"));
+
+var team = ["Marcelita", "Mariana", "Claribel", "Carlitos", "sonic"]
+for(i = 0; i < team.length; i++){
+  studentPill(team[i], $(".stu-dis"));
 }
 
-function contractChild(bar){
-  bar.data("status", "contracted");
-  bar.attr("class", "window-title expander");
-  bar.parent().next().css("display", "none");
-  bar.children().attr("class", "glyphicon glyphicon-plus");
-}
-
-// $(".headBar").on("click", function(){
-//   expandChild($(event.target));
-// });
-
-});//End of document get ready
+}); //End of document get ready
 // var studentApp = {
-// function to display the "New Student form"
-// function to close the "New Student form"
-// event handler for add a student
-
-// function to display the "Edit Student section"
-// function to close the "Edit Student section"
-// Edit hander for efit a student
 
 // function to display the "Edit form section"
 // function to close the "Edit Student section"
@@ -65,7 +85,6 @@ function contractChild(bar){
 // handler event for the student pill
 // function to request the data of the student name
 //function to display the response object inside the edit student form-box (duplicated)
-
 
 // function to create the edit object to request (post)
 // function to post the edit student info
