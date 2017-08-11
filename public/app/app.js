4ce014eab9da6cbbf923a71efc2246316df1ae6c
Karl74 <carlos.castro74@gmail.com>
2017-08-10 21:12:51 -0500

    display students pills on teams.html

--- public/app/app.js
+++ public/app/app.js
@@ -1,50 +1,8 @@
 $(document).ready(function() {
   console.log("It's alive!!!!!");
-// == | A | EVENT HANDLERS for Student.html ==================================
-  // Blue large buttons. Expands or contracts divs whith pills---------------------
-    $(".headBar").on("click", function() {
-      if ($(event.target).data("status") == "contracted") {
-        expandChild($(event.target));
-      } else if ($(event.target).data("status") == "expanded") {
-        contractChild($(event.target));
-      }
-    });
-  // New student button. Send the new student info to the database
-    $("#newStudentButton").on("click", function(){
-      event.preventDefault();
-      addNewStudent();
-      $(".form-control").val("");
-      alert("the new student was saved")
-    });
-  // Change Student Button. Send the updated information of the student to the database
-    $("#editStudentButton").on("click", function(){
-      event.preventDefault();
-      updateStudent();
-      $(".form-control").val("");
-      // hides the edit form
-      $("#editStudent").css("display", "none");
-      alert("changes were made")
-    });
 
-  // Modal x and cancel buttons. Close the modal ---------------------------------
-    $(".closeM").on("click", function(){
-        closeModal();
-    });
-
-  // Find pill. Opens the serch modal -----------------------------
-    $(".find-pill").on("click", function(){
-      $(".form-control").val("");
-      openModal();
-    });
-
-    // Find button on modal runs findStudent()
-      $("#findStudentButton").on("click", function(){
-        findStudent();
-        closeModal();
-        $("#editStudent").css("display", "block");
-      });
-
-//  == | B | FUNCTIONS ==================================
+//  == | A | FUNCTIONS ==================================
+  //  == | A.1 | FUNCTIONS for students.html==================
   // Make modal visible
     function openModal(){
       $(".modal-screen").css("display", "block");
@@ -120,15 +78,30 @@ $(document).ready(function() {
       $("#stuTeamEdit").val(students[index].team);
     }
 
-// == | C | DATA FUNCTIONS =====================
+//  == | A.1 | FUNCTIONS for teams.html==================
+  // Displays all the groups pills inside a div
+    function groupPill(groupName, parent, i){
+      var pillGroup = $("<div>");
+      pillGroup.attr("class", "group-pill pill");
+      pillGroup.data("index", i);
+      pillGroup.html(groupName);
+      parent.append(pillGroup);
+    }
+    // Makes visible a section under a given class
+    function showASection(givenClass){
+      givenClass.css("display", "block");
+    }
 
-// Set the student object and display =======================
+// == | B | DATA FUNCTIONS =====================
+
+  // Set the student object and display =======================
+  // Inlue of ajax call
     var data = [
-    	{name:"Marcela", id:7701, grade:5, tier:2, team:"" },
-    	{name:"Mariana", id:7904, grade:7, tier:2, team:"" },
+    	{name:"Marcela", id:7701, grade:5, tier:2, team:"NotAsigned" },
+    	{name:"Mariana", id:7904, grade:7, tier:2, team:"NotAsigned" },
     	{name:"Claribel", id:7101, grade:3, tier:2, team:"Patotas" },
-      {name:"Carlitos", id:7201, grade:1, tier:2, team:""},
-      {name:"Sonic", id:7301, grade:"k", tier:2, team:""}
+      {name:"Carlitos", id:7201, grade:1, tier:2, team:"NotAsigned"},
+      {name:"Sonic", id:7301, grade:"k", tier:2, team:"NotAsigned"}
     	];
 
     function Student(name, id, grade, tier, team){
@@ -146,20 +119,151 @@ $(document).ready(function() {
         return this.counter +-1;
         }
     }
-
     // Here a new array is creating with the method map and the constructor
       var students = data.map(function (array){
 	     return new Student(array.name, array.id, array.grade, array.tier, array.team);
       });
-    // Create the pills for each student in the object
-      for(i = 0; i < students.length; i++){
-        studentPill(students[i].name, $(".stu-dis"), i);
+
+      var activeTeam = "not assigned";
+      switch ($("body").data("file")) {
+        case "student":
+              console.log("this is the student file");
+              for(i = 0; i < students.length; i++){
+                studentPill(students[i].name, $(".stu-dis"), i);
+              }
+          break;
+        case "teams":
+            console.log("this is the team file");
+            // for(i = 0; i < students.length; i++){
+            //   if(students[i].team !== activeTeam){
+            //     studentPill(students[i].name, $(".stu-dis"), i);
+            //   }
+            // }
+          break;
       }
-    // Add
-      $(".student-pill").on("click", function(){
+
+// ===== | B.1 | TEAMS DATA FUNCTIONS
+    // inlue of ajax call
+    var teams = [
+      {name: "Fifth A" , grade:5 ,tier:2},
+      {name: "Fifth B" , grade:5 ,tier:2},
+      {name: "Fourth A" , grade:5 ,tier:2},
+      {name: "Fourth B" , grade:5 ,tier:2}
+    ];
+
+    for(i = 0; i <teams.length; i++){
+      groupPill(teams[i].name, $(".group-display"), i);
+    }
+
+  // Display students that do not belong to the activeTeam
+  function excludedFromActiveTeam(){
+    for(i = 0; i < students.length; i++){
+      if(students[i].team !== activeTeam){
+        studentPill(students[i].name, $(".allStu-dis"), i);
+      }
+    }
+  }
+
+  // Display students belongs to the activeTeam
+  function includedOnActiveTeam(){
+    for(i = 0; i < students.length; i++){
+      if(students[i].team == activeTeam){
+        studentPill(students[i].name, $(".teamStu-dis"), i);
+      }
+    }
+    $(".student-pill").on("click", function(){
+      updateStudentGroup($(this).data("index"));
+      $(".teamStu-dis").append($(this));
+    });
+  }
+  // Clear the student pill display from previous groups
+    function clearStudentsPills(){
+      $(".allStu-dis").empty();
+      $(".teamStu-dis").empty();
+    }
+
+  // Add or update the group of the student
+    function updateStudentGroup(index){
+      console.log(index);
+      students[index].team = activeTeam;
+      console.log(students[index]);
+
+    }
+
+// == | C | EVENT HANDLERS  ==================================
+  // == | C | EVENT HANDLERS for Student.html ==================================
+
+  // Blue large buttons. Expands or contracts divs whith pills---------------------
+    $(".headBar").on("click", function() {
+      if ($(event.target).data("status") == "contracted") {
+        expandChild($(event.target));
+      } else if ($(event.target).data("status") == "expanded") {
+        contractChild($(event.target));
+      }
+    });
+  // New student button. Send the new student info to the database
+    $("#newStudentButton").on("click", function(){
+      event.preventDefault();
+      addNewStudent();
+      $(".form-control").val("");
+      alert("the new student was saved")
+    });
+    $("#editStudentButton").on("click", function(){
+      event.preventDefault();
+      updateStudent();
+      $(".form-control").val("");
+      // hides the edit form
+      $("#editStudent").css("display", "none");
+      alert("changes were made")
+    });
+
+  // Modal x and cancel buttons. Close the modal ---------------------------------
+    $(".closeM").on("click", function(){
+        closeModal();
+    });
+
+  // Find pill. Opens the serch modal -----------------------------
+    $("#findStudentPill").on("click", function(){
+      $(".form-control").val("");
+      openModal();
+    });
+
+    // FIND BUTTON (on modal) runs findStudent()
+      $("#findStudentButton").on("click", function(){
+        findStudent();
+        closeModal();
         $("#editStudent").css("display", "block");
-        fillEditBox($(this).data("index"));
       });
+
+      // STUDENT-PILL FUNCTION(on student.html). spreads student data inside the edit box
+      // STUDENT-PILL FUNCTION(on teams.html). pending
+      // STUDENT-PILL FUNCTION(on lesson.html). pending
+      $(".student-pill").on("click", function(){
+        if($("body").data("file") == "student"){
+          $("#editStudent").css("display", "block");
+          fillEditBox($(this).data("index"));
+        } else//($("body").data("file") == "teams")
+        {
+          // activeTeam = $("#groupName").val();
+          console.log("hello");
+        }
+      });
+
+  // == | C | EVENT HANDLERS for teams.html ==================================
+      $("#createNewGroup").on("click", function(){
+        showASection($(".createNewGroup"));
+      });
+
+      $("#addStudentsButton").on("click", function(){
+        clearStudentsPills()
+        showASection($(".createAndEdit"));
+        activeTeam = $("#groupName").val();
+        console.log(activeTeam);
+        excludedFromActiveTeam();
+        includedOnActiveTeam();
+      });
+
+
 
 
 });  // End of document get ready
